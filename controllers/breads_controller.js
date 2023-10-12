1ed53fd52d548615f5f988d6da781cf74e53a0b1
loreleiesca <110075659+loreleiesca@users.noreply.github.com>
2023-10-11 21:16:40 -0700

    server side views and rendering with jsx

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,10 +2,20 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
+/*INDEX
 breads.get('/', (req, res) => {
   res.send(Bread)
+})*/
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+    {
+        breads: Bread
+      }
+      )
+  // res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
